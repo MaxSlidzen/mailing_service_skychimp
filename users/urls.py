82e023b008449ec980537bfbd539a579bb9db258
max_slidzen <slidzon94@gmail.com>
2023-09-15 16:51:54 +0300

    corrected error after verifying from own email. Added right access for login and register views by authenticated users

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from users.apps import UsersConfig
-from users.views import UserRegisterView, UserUpdateView, UserLoginView, verify, verify_success, send_verify, \
+from users.views import UserRegisterView, UserUpdateView, UserLoginView, verify, send_verify, \
     toggle_activity, UserDetailView, UserListView, UserDeleteView
 
 app_name = UsersConfig.name
@@ -16,7 +16,6 @@ urlpatterns = [
     path('user_detail_<int:pk>/', UserDetailView.as_view(), name='user_detail'),
     path('user_delete_<int:pk>/', UserDeleteView.as_view(), name='user_delete'),
     path('verify_<str:verify_key>/', verify, name='verify'),
-    path('verify_success/', verify_success, name='verify_success'),
     path('send_verify/', send_verify, name='send_verify'),
     path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
     path('user_list/', cache_page(30)(UserListView.as_view()), name='user_list'),
